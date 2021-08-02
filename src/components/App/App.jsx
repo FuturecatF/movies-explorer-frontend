5775dbc80bf7c638e23570496a441e098f24b3a1
FuturecatF <futurecatforsale@gmail.com>
2021-08-02 22:32:57 +0300

    сделать проект

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -54,7 +54,7 @@ function App() {
           <Route path="/profile">
             <Profile />
           </Route>
-          <Route path="">
+          <Route path="*">
             <PageNotFound />
           </Route>
         </Switch>
